176307e432273c7362088f36bd9acb940e46d17e
huangjiarong <203877208@qq.com>
2025-11-05 10:20:19 +0800

    删除骗子酒馆

--- liars_bar_llm/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Liars Bar LLM package."""
-
-from .game import Game
-from .player import Player
-from .game_record import GameRecord, PlayerInitialState
-
-__all__ = ["Game", "Player", "GameRecord", "PlayerInitialState"]
